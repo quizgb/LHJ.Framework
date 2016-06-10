efbd3904df22336d6bd7ae248c9912ce81c2dea2
LEEHOJUN <quizgb@naver.com>
2016-06-10 10:19:36 +0900

    1.Practice -> JSON 추

--- LHJ.Practice/FrmJson.Designer.cs
+++ LHJ.Practice/FrmJson.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "조회";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(688, 469);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // FrmJson
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(712, 522);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnSearch);
             this.Name = "FrmJson";
             this.Text = "FrmJson";
             this.ResumeLayout(false);
@@ -41,5 +64,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView listView1;
     }
 }
--- LHJ.Practice/FrmJson.cs
+++ LHJ.Practice/FrmJson.cs
@@ -3,17 +3,91 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace LHJ.Practice
 {
     public partial class FrmJson : Form
     {
+        public class Issue
+        {
+            public string Subject { get; set; }
+            public string Done { get; set; }
+            public string Author { get; set; }
+        }
+
         public FrmJson()
         {
             InitializeComponent();
+        }
+
+        private string Request_Json()
+        {
+            this.listView1.Items.Clear();
+
+            string result = null;
+            string url = "http://www.redmine.org/issues.json";
+            Console.WriteLine("url : " + url);
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return result;
+        }
+
+        private void ParseJson(String json)
+        {
+            listView1.BeginUpdate();
+
+            List<Issue> issues = new List<Issue>();
+            JObject obj = JObject.Parse(json);
+            JArray array = JArray.Parse(obj["issues"].ToString());
+
+            foreach (JObject itemObj in array)
+            {
+                Issue issue = new Issue();
+                issue.Subject = itemObj["subject"].ToString();
+                issue.Done = itemObj["done_ratio"].ToString();
+                issue.Author = itemObj["author"]["name"].ToString();
+                issues.Add(issue);
+            }
+
+            foreach(Issue isu in issues)
+            {
+                ListViewItem lvi = new ListViewItem(isu.Subject);
+                lvi.SubItems.Add(isu.Done);
+                lvi.SubItems.Add(isu.Author);
+
+                listView1.Items.Add(lvi);
+            }
+
+            this.listView1.Columns.Add("Subject",  200/*가로사이즈*/, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("Done", 200/*가로사이즈*/, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("Author", 200/*가로사이즈*/, HorizontalAlignment.Left);
+
+            listView1.EndUpdate();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.ParseJson(this.Request_Json());
         }
     }
 }
