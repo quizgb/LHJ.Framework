d0c9d5e0f923d7bc1ef1d331d06e30f1cc191974
LEEHOJUN <quizgb@naver.com>
2017-01-20 15:52:46 +0900

    1

--- LHJ.ZipCodeSearch/ZipSearchFrm.cs
+++ LHJ.ZipCodeSearch/ZipSearchFrm.cs
@@ -155,7 +155,7 @@ namespace LHJ.ZipCodeSearch
             string subUrl = string.Format("ServiceKey={0}&searchSe={1}&srchwrd={2}&countPerPage={3}&currentPage={4}",
                                            "p6tFjeXc4J6rP3pA2DUzI5%2FFopaEOTfA4bfqSPFTCLZXO1Y4GQYWs1iZLYQNEWgHQzYNBXdNULz61rovjSnNaw%3D%3D",
                                            "dong",
-                                           "가좌동",
+                                           "가좌마을",
                                            "10",
                                            "1");
             string url = "http://openapi.epost.go.kr/postal/retrieveLotNumberAdressAreaCdService/retrieveLotNumberAdressAreaCdService/getDetailListAreaCd?" + subUrl;
